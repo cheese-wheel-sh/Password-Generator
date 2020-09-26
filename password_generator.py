29bf602e50c1e47874d0f54fc9c1fae667adc56d
itsthooor <71508362+itsthooor@users.noreply.github.com>
2020-09-26 17:51:39 +0200

    Update password_generator.py
    Added:
       Code cleanup
       clear() function
       Errors fixed
       Better safemode algorythm
       New tries counter
    Soon:
       GUI
       Custom characters for password

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,21 @@
 import re
+import os
 import sys
 import time
 import random
 import string
 import argparse
 import datetime
+from subprocess import call
 
 class Variables():
     def __init__(self):
+        self._try = 1
         self.final_password = 0
         self.weak = string.ascii_letters
         self.normal = string.ascii_letters + string.digits
         self.strong = string.ascii_letters + string.digits + string.punctuation
+        self.custom = None
         self.parser = argparse.ArgumentParser("Password Generator", description='A simple command line password generator by itsthooor', usage='password_generatory.py -l LENGTH [1 or higher] -m MODE [weak, normal, strong] -sm SAFEMODE [(y)es or (n)o]', epilog='Github -> https://github.com/itsthooor')
         self.parser.add_argument('-l' ,'--length', dest='length' , metavar='LENGTH', help='Length of the password', type=int, default=8)
         self.parser.add_argument('-m' ,'--mode' , dest='mode', metavar='MODE', help='Sets the passwords security level', type=str, default='normal', choices=['weak', 'normal', 'strong'])
@@ -27,7 +31,15 @@ variables = Variables()
 timer1 = Timers()
 timer2 = Timers()
 
-def timer_start(mode):
+def clear():
+    _ = call('clear' if os.name =='posix' else 'cls')
+
+def tries(silent=False):
+    variables._try += 1
+    if not silent:
+        print('Try nr.:',variables._try)
+
+def timer_st(mode):
     if mode == 'generate':
         timer1.timer_start = time.perf_counter()
     if mode == 'print':
@@ -44,43 +56,54 @@ def timer_stop(mode):
         timer2.timer_time = str(datetime.timedelta(seconds=round(timer_print_eq)))
 
 def generate(mode, chars, length, safemode):
-    timer_start('generate')
+    timer_st('generate')
+    if mode == 'weak':
+        print('No safemode for weak available!')
+        time.sleep(1)
     if safemode in ('Yes', 'yes', 'Y', 'y'):
         print('Safemode activated!')
         password = ''.join(random.choice(chars) for x in range(length))
         if mode == 'normal':
             intgen = re.findall(r'\d+', str(password))
             while len(intgen) < (length / 3):
+                clear()
+                tries()
                 password = ''.join(random.choice(chars) for x in range(length))
                 intgen = re.findall(r'\d+', str(password))
         if mode == 'strong':
             lettergenerator = re.findall("[A-Z]+", str(password))
             intgen = re.findall(r'\d+', str(password))
-            while len(lettergenerator) < (length / 3):
+            while len(lettergenerator) < (length / 3) and len(lettergenerator) > (length / 2):
+                clear()
+                tries()
                 password = ''.join(random.choice(chars) for x in range(length))
                 lettergenerator = re.findall("[A-Z]+", str(password))
-            while len(intgen) < (length / 3):
+            while len(intgen) < (length / 3) and len(intgen) > (length / 2):
+                clear()
+                tries()
                 password = ''.join(random.choice(chars) for x in range(length))
                 intgen = re.findall(r'\d+', str(password))
         variables.final_password = password
     else:
+        tries(True)
         password = ''.join(random.choice(chars) for x in range(length))
         variables.final_password = password
     timer_stop('generate')
-    timer_start('print')
+    timer_st('print')
+    clear()
     return password
 
 if __name__ == "__main__":
     if variables.args.mode == 'weak':
-        print('Weak Password with', variables.args.length,'characters =', generate('weak', variables.weak, variables.args.length, variables.args.safemode), '\nGenerated in', timer1.timer_time)
+        print('Weak Password with', variables.args.length,'characters =', generate('weak', variables.weak, variables.args.length, variables.args.safemode), '\nGenerated in', timer1.timer_time, 'with', variables._try, 'tries')
     elif variables.args.mode == 'normal':
-        print('Normal Password with', variables.args.length,'characters =', generate('normal', variables.normal, variables.args.length, variables.args.safemode), '\nGenerated in', timer1.timer_time)
+        print('Normal Password with', variables.args.length,'characters =', generate('normal', variables.normal, variables.args.length, variables.args.safemode), '\nGenerated in', timer1.timer_time, 'with', variables._try, 'tries')
     else:
-        print('Strong Password with', variables.args.length,'characters =', generate('strong', variables.strong, variables.args.length, variables.args.safemode), '\nGenerated in', timer1.timer_time)
+        print('Strong Password with', variables.args.length,'characters =', generate('strong', variables.strong, variables.args.length, variables.args.safemode), '\nGenerated in', timer1.timer_time, 'with', variables._try, 'tries')
     timer_stop('print')
     print('Time for printing:',timer2.timer_time)
     answer = input('Wanna print it to a text file? [y/n] -> ')
     if answer == 'y':
         with open('password.txt', 'w') as f:
             f.write(str(variables.final_password))
-    f.close()
+            f.close()
